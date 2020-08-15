26163722d0773a9b3709950b0f62a543091c3897
Pallavi Datta <plv.datta@gmail.com>
2020-08-15 19:55:17 +0530

    added travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello There!
+          /learn react/i
         </a>
       </header>
     </div>
